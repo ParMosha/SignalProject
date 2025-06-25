7e5edebd9d04b2de90ec92dcd5f931afcd81b372
ParMosha <160258768+ParMosha@users.noreply.github.com>
2025-06-25 13:03:02 +0330

    some adjusments

--- Detection.py
+++ Detection.py
@@ -8,8 +8,9 @@ from io import BytesIO
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 
+
 def get_faster_rcnn_model(num_classes):
-    model = torchvision.models.detection.fasterrcnn_resnet50_fpn(weights="DEFAULT")
+    model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
     in_features = model.roi_heads.box_predictor.cls_score.in_features
     model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
     return model
@@ -36,21 +37,22 @@ def detect_objects(model, image, threshold=0.7):
     return results, prediction
 
 def visualize_detections(image, detections):
-    fig, ax = plt.subplots(1)
+    _, ax = plt.subplots(1)
     ax.imshow(image)
     for det in detections:
         x1, y1, x2, y2 = det['box']
-        rect = patches.Rectangle((x1, y1), x2 - x1, y2 - y1, linewidth=2, edgecolor='r', facecolor='none')
+        rect = patches.Rectangle((x1, y1), x2 - x1, y2 - y1, linewidth=2, edgecolor='r', facecolor='none',alpha=0.5)
         ax.add_patch(rect)
         ax.text(x1, y1, f'Label: {det["label"]}, Score: {det["score"]:.2f}',
-                bbox=dict(facecolor='yellow', alpha=0.5), fontsize=8, color='black')
+                bbox=dict(facecolor='yellow', alpha=0.1), fontsize=8, color='black')
     plt.axis('off')
     plt.show()
 
 if __name__ == "__main__":
-    num_classes = int(input("Enter number of classes (including background): "))
+    # num_classes = int(input("Enter number of classes (including background): "))
+    num_classes = 2
     model = get_faster_rcnn_model(num_classes)
-    image_url = "https://raw.githubusercontent.com/ParMosha/SignalProject/main/2.jpg"
+    image_url = "https://raw.githubusercontent.com/ParMosha/SignalProject/main/9.jpg"
     image = load_image_from_url(image_url)
     threshold = 0.5
     detections, prediction = detect_objects(model, image, threshold)
