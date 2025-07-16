a06d18db2b7771404742b215500c7b174df5343d
ParMosha <160258768+ParMosha@users.noreply.github.com>
2025-07-16 16:00:40 +0330

    finaly works

--- Detection.py
+++ Detection.py
@@ -1,7 +1,9 @@
 import cv2
-import torch
 from ultralytics import YOLO
 import urllib.request
+import tempfile
+import os
+import matplotlib.pyplot as plt
 
 def load_model(model_path):
     """
@@ -38,20 +40,37 @@ def download_image(url, save_path):
         print(f"Image downloaded and saved to {save_path}")
     except Exception as e:
         print(f"Error downloading image: {e}")
-    
+
 def main():
     model_url = "https://raw.githubusercontent.com/ParMosha/SignalProject/main/yolo11s.pt"
-    model_path = "downloaded_weights.pt"
-    urllib.request.urlretrieve(model_url, model_path)
     image_url = "https://raw.githubusercontent.com/ParMosha/SignalProject/main/image.png"
-    image_path = 'input_image.jpg'  # Path to the input image
-    urllib.request.urlretrieve(image_url, image_path)
-    model = load_model(model_path)
-    if model:
-        results = detect_objects(model, image_path)
-        if results:
-            print("Detection results:", results)
-        else:
-            print("No objects detected or an error occurred.")
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        model_path = os.path.join(tmpdirname, "downloaded_weights.pt")
+        image_path = os.path.join(tmpdirname, "input_image.jpg")
+        urllib.request.urlretrieve(model_url, model_path)
+        urllib.request.urlretrieve(image_url, image_path)
+        model = load_model(model_path)
+        if model:
+            results = detect_objects(model, image_path)
+            if results:
+                print("Detection results:", results)
+                image = cv2.imread(image_path)
+                if image is None:
+                    raise ValueError("Image not found or unable to read.")
+                
+                for result in results:
+                    boxes = result.boxes.xyxy.cpu().numpy()
+                    for box in boxes:
+                        x1, y1, x2, y2 = map(int, box[:4])
+                        cv2.rectangle(image, (x1, y1), (x2, y2), (0, 255, 0), 2)
+                plt.figure(figsize=(10, 10))
+                plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+                plt.axis('off')
+                plt.show()
+            
+            else:
+                print("No objects detected or an error occurred.")
+
 if __name__ == "__main__":
     main()
+
