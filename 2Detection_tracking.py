cdfa6bb6068220ecd8d82634f9603dd32f171310
Mohammadmosayebzadeh <168230876+Mohammadmosayebzadeh@users.noreply.github.com>
2025-07-21 23:41:40 +0330

    improve code 1.1

--- 2Detection_tracking.py
+++ 2Detection_tracking.py
@@ -6,7 +6,7 @@ class YOLOMultiTracker:
     def __init__(
         self,
         model_path: str = 'yolov8n.pt',
-        confidence_threshold: float = 0.5,
+        confidence_threshold: float = 1,
         detection_interval: float = 0.1
     ):
         self.model = YOLO(model_path)
